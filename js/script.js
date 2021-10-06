c045b190f7b5f048a9ae0dffd0a800fe2e0a88e3
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2021-10-06 23:45:46 +0800

    create movingLine function as page load

--- js/script.js
+++ js/script.js
@@ -7,13 +7,20 @@ var contentHolder = document.getElementById('content-holder');
 var navLink = document.querySelectorAll('.nav-link');
 var mainBanner = document.getElementById('main-banner');
 var aboutSection = document.getElementById('about-section');
-console.log(aboutSection)
+console.log(aboutSection);
 var skillSection = document.getElementById('skill-section');
 var portfolioSection = document.getElementById('portfolio-section');
 var contactSection = document.getElementById('contact-section');
+var movingLine = document.getElementById('moving-line');
+var iteration = 1;
 
 var isMenuActive = false;
 var isScrolling = false;
+
+var movingBox = {
+  posX: 65,
+  posY: 15,
+};
 
 function toggleMenu() {
   if (isMenuActive) {
@@ -74,8 +81,10 @@ function scrollStop(callback, refresh = 100) {
 window.addEventListener('scroll', onScroll);
 
 function onScroll(event) {
-
-  if (window.scrollY + 80 >= aboutSection.offsetTop && window.scrollY + 65 < aboutSection.offsetTop + aboutSection.clientHeight) {
+  if (
+    window.scrollY + 80 >= aboutSection.offsetTop &&
+    window.scrollY + 65 < aboutSection.offsetTop + aboutSection.clientHeight
+  ) {
     navLink.forEach((list) => {
       list.style.color = 'var(--secondary-color)';
     });
@@ -83,5 +92,93 @@ function onScroll(event) {
     navLink.forEach((list) => {
       list.style.color = 'black';
     });
+  }
+}
+
+var movingLineInterval = setInterval(moveLine, 25);
+
+function moveLine() {
+  var x = 10;
+  var y = 10;
+
+  if (iteration === 1) {
+    movingBox.posX += x;
+    movingLine.style.left = `${movingBox.posX}px`;
+    console.log('here1');
+    if (movingBox.posX >= mainBanner.clientWidth - 115) {
+      clearInterval(movingLineInterval);
+      iteration++;
+      movingLine.style.transition = `all 100ms`;
+      movingLine.style.width = `5px`;
+      movingLine.style.height = `2px`;
+      movingLine.style.left = `${movingBox.posX + 95}px`;
+      setTimeout(() => {
+        movingLine.style.width = `2px`;
+        movingLine.style.height = `100px`;
+        setInterval(moveLine, 25);
+      }, 100);
+    }
+    // return
+  }
+
+  if (iteration === 2) {
+    console.log('here2');
+    movingBox.posY += y;
+    movingLine.style.top = `${movingBox.posY}px`;
+    if (movingBox.posY >= mainBanner.clientHeight - 45) {
+      clearInterval(movingLineInterval);
+      iteration++;
+      movingLine.style.transition = `all 100ms`;
+      movingLine.style.width = `5px`;
+      movingLine.style.height = `2px`;
+      movingLine.style.top = `${movingBox.posY + 25}px`;
+      movingLine.style.left = `${movingBox.posX - 35}px`;
+      setTimeout(() => {
+        movingLine.style.width = `100px`;
+        movingLine.style.height = `2px`;
+        // setInterval(moveLine, 10);
+      }, 100);
+    }
+      // return;
+  }
+  if (iteration === 3) {
+    console.log('here3');
+    movingBox.posX -= x;
+    movingLine.style.left = `${movingBox.posX}px`;
+    if (movingBox.posX <= 15) {
+      clearInterval(movingLineInterval);
+      iteration++;
+      movingLine.style.transition = `all 100ms`;
+      movingLine.style.width = `5px`;
+      movingLine.style.height = `2px`;
+      movingLine.style.top = `${movingBox.posY - 75}px`;
+      movingLine.style.left = `${15}px`;
+      setTimeout(() => {
+        movingLine.style.width = `2px`;
+        movingLine.style.height = `100px`;
+        // setInterval(moveLine, 10);
+      }, 100);
+    }
+      // return;
+  }
+  if (iteration === 4) {
+    console.log('here4');
+    movingBox.posY -= y;
+    movingLine.style.top = `${movingBox.posY}px`;
+    if (movingBox.posY <= 15) {
+      clearInterval(movingLineInterval);
+      iteration = 1;
+      movingLine.style.transition = `all 100ms`;
+      movingLine.style.width = `5px`;
+      movingLine.style.height = `2px`;
+      movingLine.style.top = `${15}px`;
+      movingLine.style.left = `${65}px`;
+      setTimeout(() => {
+        movingLine.style.width = `100px`;
+        movingLine.style.height = `2px`;
+        // setInterval(moveLine, 10);
+      }, 100);
+    }
+      // return;
   }
 }
