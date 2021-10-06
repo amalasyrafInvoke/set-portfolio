a7bc367c1c54b2b01608dd60d395c1ba15650567
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2021-10-06 20:54:22 +0800

    fix container width and add skill section content

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var navbarBrand = document.getElementById('navbarBrand');
 var menuImg = document.getElementById('menuImage');
 var contentHolder = document.getElementById('content-holder');
 var isMenuActive = false;
+var isScrolling = false;
 
 function toggleMenu() {
   if (isMenuActive) {
@@ -15,7 +16,7 @@ function toggleMenu() {
       navBarNav.style.display = 'none';
       navbarBrand.style.display = 'block';
     }, 750);
-    document.querySelector('body').style.overflow = 'auto';
+    // document.querySelector('body').style.overflow = 'auto';
     document.querySelector('html').style.overflow = 'auto';
     isMenuActive = false;
     return;
@@ -26,9 +27,37 @@ function toggleMenu() {
     menuImg.classList.remove('animate__animated', 'animate__fadeOutTopRight');
     menuImg.classList.add('animate__animated', 'animate__fadeInTopRight');
     navbarBrand.style.display = 'none';
-    document.querySelector('body').style.overflow = 'hidden';
+    // document.querySelector('body').style.overflow = 'hidden';
     document.querySelector('html').style.overflow = 'hidden';
     isMenuActive = true;
     return;
   }
+}
+
+scrollStop(() => {
+  navbarBrand.style.opacity = '1';
+})
+
+function scrollStop (callback, refresh = 100) {
+
+	// Make sure a valid callback was provided
+	if (!callback || typeof callback !== 'function') return;
+
+	// Setup scrolling variable
+	let isScrolling;
+
+	// Listen for scroll events
+	window.addEventListener('scroll', function (event) {
+
+    navbarBrand.style.opacity = '0';
+    navbarBrand.style.transition = 'all 500ms';
+
+		// Clear our timeout throughout the scroll
+		window.clearTimeout(isScrolling);
+
+		// Set a timeout to run after scrolling ends
+		isScrolling = setTimeout(callback, refresh);
+
+	},false);
+
 }
